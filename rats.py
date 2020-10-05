c7290f60505192453218a8eed13f07a09ed7ae59
Manoj Kumar <62828023+shaukeencoder@users.noreply.github.com>
2020-10-05 08:40:53 +0530

    Updated

--- rats.py
+++ rats.py
@@ -1,40 +1,23 @@
-# project 01 its the end of course
-# making the "Atrene" project automations of tasks just in 5yrs
-# install pyttsx3 module
-import pyttsx3                                             #for voice of atrene
-import speech_recognition as sr                            #to recogonise what i speak
-import wikipedia                                           #this is just wikipedia and its results
+import pyttsx3                                             
+import speech_recognition as sr                            
+import wikipedia                                           
 import webbrowser
-import os                                                  #to open files through their path
-import random
-import smtplib                                             #for this we have to enable less secured in gmail
-from googletrans import Translator                         #lets see will it work or not
+import os                                                  
+import random                                            
+from googletrans import Translator                        
 translator = Translator()
 import datetime
 from gtts import gTTS
 import requests
-import json                                                #for telling news
-# from covid import Covid
-# covid = Covid()
-# covid.get_data()
-# active = covid.get_total_active_cases()
-# confirmed = covid.get_total_confirmed_cases()
-# recovered = covid.get_total_recovered()
-# deaths = covid.get_total_deaths()
-# covid = Covid(source="worldometers")
+import json                                             
 
-
-#Here is the atrene voice  begins
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
-# print(voices[1].id)
 
-#Here is the speak function
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
 
 def wishme():
     hour =int(datetime.datetime.now().hour)
@@ -44,21 +27,21 @@ def wishme():
 
     if hour>=0 and hour<=12:
         speak(" Good Morning Sir!")
-        speak("I am Atrene, your personal voice assistant")
+        speak("I am Jinni, your personal voice assistant")
         speak(f"Its {day} morning ")
         speak(f" And ,the time is {stTime}")
         speak("How may i help you?")
 
     elif hour>=12 and hour<=16:
         speak("Good Afternoon Sir!")
-        speak("I am Atrene,your personal voice assistant")
+        speak("I am Jinni,your personal voice assistant")
         speak(f"It's {day} afternoon")
         speak(f" And Sir,the time is {stTime}")
         speak("How may i help you?")
 
     else:
         speak("Good evening Sir!")
-        speak("I am Atrene,your personal voice assistant")
+        speak("I am Jinni,your personal voice assistant")
         speak(f"It's {day} evening")
         speak(f" And,the time is {stTime}")
         speak("How may i help you?")
@@ -67,28 +50,17 @@ def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("listening..")
-        r.pause_threshold = 1 #see its documnetation pree ctrl+click
+        r.pause_threshold = 1 
         audio = r.listen(source)
     try:
         print("Recognising...")
         query = r.recognize_google(audio,language='en-in')
         print(f"user said: {query} \n")
     except Exception as e:
-        # print(e)
         print("say that again please...")
         return "None"
     return query
 
-    # Mail Function
-def sendemail(to,content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.login('manojsheokand111@gmail.com','password')
-    server.sendmail('youramil@gmail.com',to,content)
-    server.close()
-
-#Here is somethinf to light the mood
 def joke():
     joknum=random.randint(0,1)
     if joknum==0:
@@ -100,58 +72,30 @@ def joke():
     else:
         exit()
 
-def coronacases():
-    speak("Here the total cases in the world")
-
 def trans():
-
     speak("please speak what do you want to translate, and i will translate it in your desired language")
     t = sr.Recognizer()
     with sr.Microphone() as source:
         print("listening..")
-        t.pause_threshold = 1  # see its documentation press ctrl+click
+        t.pause_threshold = 1  
         audio = t.listen(source)
     try:
-
         print("Recognising...")
         txt = t.recognize_google(audio, language='en-in')
-        # print(f"user want to translate: {txt} \n")
-
         with sr.Microphone() as source:
             speak("In which language you want to translate")
             print("listening..")
-            t.pause_threshold = 1  # see its documentation press ctrl+click
+            t.pause_threshold = 1  
             audio = t.listen(source)
             print("Recognising......")
             lang = t.recognize_google(audio, language='en-in')
             print(f"user want to translate: {txt} \n")
             print(f"Language chosen for translation {lang} ")
             speak("please wait working on translation..")
-        # if "hindi"==lang or "Hindi" == lang:
-        #         print(lang)
-        #         translations = translator.translate(txt, dest='hi')
-        # elif "urdu"==lang or "Urdu" == lang:
-        #         print(lang)
-        #         translations = translator.translate(txt, dest='ur')
-        # elif "Polish"==lang or "polish"== lang:
-        #         print(lang)
-        #         translations = translator.translate(txt, dest='pl')
-        # else:
-        #         speak("Didnt  recognise")
-        #
-
-
-
+       
         if 'Hindi' == lang or 'hindi' == lang:
-            translations = translator.translate(txt, dest='hi')
-            # print(lang)
-            # trans_result = translations.text
-            # speak("translation is on your screen")
-            # print(trans_result)
-            # tran_ans = t.recognize_google(audio, language='hi')
-            # speak(trans_result)
+            translations = translator.translate(txt, dest='hi')            
         elif 'Urdu' == lang or 'urdu'== lang:
-            # print(lang)
             translations = translator.translate(txt, dest='ur')
         elif 'Hebrew' == lang or 'hebrew' == lang:
             translations = translator.translate(txt, dest='he')
@@ -265,43 +209,26 @@ def trans():
             translations = translator.translate(txt, dest='no')
 
         else:
-            speak("Try Again! either it is not available or you did not speak right ")
-            # translations = translator.translate(txt, dest='hi')
-            # speak(" Sorry I did not find your language so i converted it in hindi")
-            # translations = translator.translate(txt, dest='hi')
-            # for translation in translations:
-            #
-            #     print(translation.text)
-            #     speak(translation.text)
-            # return speak(translation.text)
-            # print(translations)
+            speak("Try Again! either it is not available or there was some recognition issue ")
+            
         speak("Translation is on your screen..")
         print(f"Translation language is {lang}")
         print(translations.text)
 
-        # reslt=translations.text
+       
         tranres = pyttsx3.init('sapi5')
         voices = tranres.getProperty('voices')
-        # print(voices[1].id)
         tranres.setProperty('voice', voices[1].id)
         tranres.say(translations.text)
         tranres.runAndWait()
-
-        # language='hi'
-        # myobj=gTTS(text=reslt,lang=language,slow=False)
-        # myobj.save("tr.mp3")
-        # os.system("mpg321 tr.mp3")
-        # speak(translations.text)
     except Exception as e:
         print(e)
         print("say that again please...")
         return "None"
-    # return txt
-#     it takes microphone input from the user
+
 if __name__ == "__main__":
     wishme()
     while True:
-        #logic for executing the tasks
         query = takecommand().lower()
         if 'wikipedia' in query:
             speak('Searching Results...')
@@ -312,8 +239,6 @@ if __name__ == "__main__":
             speak(results)
 
         elif 'which language you can translate' in query:
-            print("I can translate in hindi,spanish,urdu,punjabi,italian,kannad,french,german,chinese,korean,marathi,tamil,bengali,malayalam,japanese,latin,russian,arabic,persian"
-                  "greek,turkish,romanian and many more")
             speak("I can translate in hindi,spanish,urdu,punjabi,italian,kannad,french,german,chinese,korean,marathi,tamil,bengali,malayalam,japanese,latin,russian,arabic,persian"
                   "greek,turkish,romanian and many more")
         elif 'translate' in query:
@@ -327,11 +252,11 @@ if __name__ == "__main__":
             speak("I can speak,recognize,search wikipedia results,open google for you,youtube,websites like code chef ,geeksforgeeks,github,can play music"
                   "open notepad and the most recent feature i can do translation more than 30 languages ")
         elif 'your birthplace' in query:
-            speak("Sir i was developed in a room by a programmer who is a kunjean, his  name is Manoj 2 3 ")
+            speak("Sir i was made a shaukeen coder")
         elif 'what is your age' in query:
             speak("I keep on update myself so i have no age")
-        elif 'quit' in query:
-            speak("Quitting the program ")
+        elif 'exit' in query:
+            speak("Exiting the program ")
             exit()
         elif 'rap god'in query:
             speak("playing rap god")
@@ -341,7 +266,6 @@ if __name__ == "__main__":
             webbrowser.open("https://www.youtube.com/watch?v=G7RW-KVDeEo")
         elif 'joke' in query:
             speak("Ohh! you want me to be funny ")
-            speak("Okay! let me tell you joke")
             joke()
         elif 'open youtube' in query:
             speak("opening youtube")
@@ -359,10 +283,10 @@ if __name__ == "__main__":
             speak("opening codechef")
             webbrowser.open("https://www.codechef.com/")
         elif 'play music' in query:
-            music_dir ='F:\\Music'
+            #Add your music files path below
+            music_dir =''              
             songs=os.listdir(music_dir)
             speak("playing music...")
-            # print(songs)
             num=random.randint(0,10)
             os.startfile(os.path.join(music_dir, songs[num]))
         elif 'time' in query:
@@ -370,11 +294,11 @@ if __name__ == "__main__":
             speak(f"Sir,the time is {strTime}")
         elif 'open code' in query:
             speak("opening vs code")
-            path="F:\\Microsoft VS Code\\Code.exe"
+            path=""
             os.startfile(path)
         elif 'notepad' in query:
             speak("opening notepad")
-            npath="C:\\Users\\HP\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad.lnk"
+            npath=""
             os.startfile(npath)
         elif 'word' in query:
             speak("opening ms word")
@@ -382,7 +306,7 @@ if __name__ == "__main__":
             os.startfile(mpath)
         elif 'turbo' in query:
             speak("opening code blocks...")
-            cpath = "C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe"
+            cpath = ""
             os.startfile(cpath)
         elif 'open instagram' in query:
             speak("opening instagram"
@@ -392,68 +316,17 @@ if __name__ == "__main__":
             speak("opening facebook.."
                   "please login you details there")
             webbrowser.open("https://www.facebook.com/")
-        elif 'news'  in query:
-              url = "http://newsapi.org/v2/top-headlines?sources=the-hindu&apiKey=a85299c00e2e424ea630b7b3b591e50b"
+        elif 'news' or 'headlines'  in query:
+            #Add your own API Key
+              url = ""
               news=requests.get(url).text
               news_dict=json.loads(news)
               speak("please enter number of headlines you want to listen")
               headnumb=int(input("Enter here\t"))
               speak("Here are the top headlines")
-            #   print(news_dict["articles"])
-            #   arts=news_dict["articles"]
+          
               for i in range(0,headnumb):
                   print(i+1,news_dict['articles'][i]['title'])
                   speak(news_dict['articles'][i]['title'])
-                #   speak("next news")
-        elif 'headline' in query:
-              url = "http://newsapi.org/v2/top-headlines?sources=the-hindu&apiKey=a85299c00e2e424ea630b7b3b591e50b"
-              news=requests.get(url).text
-              news_dict=json.loads(news)
-              speak("please enter number of headlines you want to listen")
-              headnumb=int(input("Enter here\t"))
-              speak("Here are the top headlines")
-            #   print(news_dict["articles"])
-            #   arts=news_dict["articles"]
-              for i in range(0,headnumb):
-                  print(i+1,news_dict['articles'][i]['title'])
-                  speak(news_dict['articles'][i]['title'])
-                #   speak("next news")
-        elif 'headlines'  in query:
-              url = "http://newsapi.org/v2/top-headlines?sources=the-hindu&apiKey=a85299c00e2e424ea630b7b3b591e50b"
-              news=requests.get(url).text
-              news_dict=json.loads(news)
-              speak("please enter number of headlines you want to listen")
-              headnumb=int(input("Enter here\t"))
-              speak("Here are the top headlines")
-            #   print(news_dict["articles"])
-            #   arts=news_dict["articles"]
-              for i in range(0,headnumb):
-                  print(i+1,news_dict['articles'][i]['title'])
-                  speak(news_dict['articles'][i]['title'])
-                #   speak("next news")
-        elif 'radha soami shabad' in query:
-            speak("playing shabad")
-            webbrowser.open("https://www.youtube.com/watch?v=ijP4Lb_egEU")
-
-        elif 'email to mk' in query:
-            try:
-                speak("what should i write sir?")
-                content=takecommand()
-                to="guidingefforts111@gmail.com"
-                sendemail(to,content)
-                speak("Email sent successfully!")
-            except Exception as e:
-                print(e)
-                speak("Sorry soemthing went wrong, I am unable to send it")
-
-
-
-
-
-    # speak("Yo")
-
-
-
-
-
-
+               
+ 
